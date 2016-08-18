4f0f916f06fb4800f9dc11063e2cdf782a4c36ba
Joel Sjöberg <joelsjoberg3@gmail.com>
2016-08-18 14:14:39 +0300

    Main class

---
+++ MainGame.java
@@ -0,0 +1,75 @@
+package connected;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class MainGame extends JFrame{
+    
+    private int fps = 0;
+    private final int width = 800;
+    private final int height = width / 16 * 9;
+    private boolean running = false;
+    private Screen screen;
+    private KeyBoard k = new KeyBoard();
+    public int msPerFrame = 12;
+    private void begin(){    
+        screen = new Screen(width, height, msPerFrame);
+        add(screen);
+        pack();
+        addKeyListener(k);
+        setSize(new Dimension(width, height));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+    // Game loop
+    public void run() {
+        
+        double prev = System.currentTimeMillis();
+        double lag = 0.0;
+        double current;
+        double elapsed;
+        
+        double fpsTimeBegin = System.currentTimeMillis();
+        double fpsEnd;
+        while(running){
+            current = System.currentTimeMillis();
+            elapsed = current - prev;
+            prev = current;
+            lag += elapsed;
+            
+// input here
+            
+            if(lag >= msPerFrame){
+                screen.render(k.getKeys());
+                lag -= msPerFrame;
+                
+// FPS counter
+                fpsEnd = System.currentTimeMillis();
+                fps++;
+                if(fpsEnd - fpsTimeBegin > 1000){
+                    setTitle(Integer.toString(fps));
+                    fps = 0;
+                    fpsTimeBegin = fpsEnd;
+                }
+            } 
+        }
+    }
+    
+    private void start(){
+        running = true;
+    }
+    private void stop(){
+        running = false;
+    }
+    public int getTimeMs(){
+        return msPerFrame;
+    }
+    
+    public static void main(String[] args){
+        MainGame game = new MainGame();
+        game.begin();
+        game.start();
+        game.run();
+    }
+}
